b2c63b0ddcd315c8c8953a66fc5bc02872596f02
Winner Abiodun <80297978+winnerabiodun1@users.noreply.github.com>
2021-05-07 11:21:31 +0100

    model created

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Profile(models.Model):
+    username = models.CharField(max_length=75)
+    password = models.CharField(max_length=512)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.username
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def index(request):
+    payload={'message': 'Hello World!'}
+    response = JsonResponse(payload)
+    return response
+
